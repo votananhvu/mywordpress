fa13f2f787cbc5d74d5c20e04ef40218d8ea560d
votananhvu <85145333+votananhvu@users.noreply.github.com>
2021-09-26 22:34:47 +0700

    thaydoi3

--- wp-login.php
+++ wp-login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="vi">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log In &lsaquo; Bongda.com.vn &#8212; WordPress</title>
+	<title>Đăng nhập &lsaquo; Smartphone &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="https://votananhvu.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" media="all">
@@ -12,51 +12,51 @@
 <link rel="stylesheet" id="login-css" href="https://votananhvu.github.io/mywordpress/wp-admin/css/login.min.css?ver=5.8.1" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/06/cropped-Logo-Bong-Da-02-1-32x32.jpg" sizes="32x32">
-<link rel="icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/06/cropped-Logo-Bong-Da-02-1-192x192.jpg" sizes="192x192">
-<link rel="apple-touch-icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/06/cropped-Logo-Bong-Da-02-1-180x180.jpg">
-<meta name="msapplication-TileImage" content="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/06/cropped-Logo-Bong-Da-02-1-270x270.jpg">
+	<link rel="icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/09/cropped-logosmartphone-1-32x32.jpg" sizes="32x32">
+<link rel="icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/09/cropped-logosmartphone-1-192x192.jpg" sizes="192x192">
+<link rel="apple-touch-icon" href="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/09/cropped-logosmartphone-1-180x180.jpg">
+<meta name="msapplication-TileImage" content="https://votananhvu.github.io/mywordpress/wp-content/uploads/2021/09/cropped-logosmartphone-1-270x270.jpg">
 	</head>
-	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<body class="login no-js login-action-login wp-core-ui  locale-vi">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
 	</script>
 		<div id="login">
-		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
 	
 		<form name="loginform" id="loginform" action="https://votananhvu.github.io/mywordpress/wp-login.php" method="post">
 			<p>
-				<label for="user_login">Username or Email Address</label>
+				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
 			</p>
 
 			<div class="user-pass-wrap">
-				<label for="user_pass">Password</label>
+				<label for="user_pass">Mật khẩu</label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hiện mật khẩu">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
 									<input type="hidden" name="redirect_to" value="https://votananhvu.github.io/mywordpress/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-				<a href="https://votananhvu.github.io/mywordpress/wp-login.php?action=register">Register</a> | 				<a href="https://votananhvu.github.io/mywordpress/tai-khoan/lost-password/">Lost your password?</a>
+				<a href="https://votananhvu.github.io/mywordpress/wp-login.php?action=register">Đăng ký</a> | 				<a href="https://votananhvu.github.io/mywordpress/tai-khoan/lost-password/">Bạn quên mật khẩu?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://votananhvu.github.io/mywordpress/">&larr; Go to Bongda.com.vn</a>		</p>
-		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://votananhvu.github.io/mywordpress/">HomePage</a></div>	</div>
+			<a href="https://votananhvu.github.io/mywordpress/">&larr; Quay lại Smartphone</a>		</p>
+			</div>
 	<script src="https://votananhvu.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
 <script src="https://votananhvu.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
@@ -71,14 +71,14 @@ var _zxcvbnSettings = {"src":"https:\/\/votananhvu.github.io\/mywordpress\/wp-in
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
 <script id="password-strength-meter-js-extra">
-var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rất yếu","bad":"Yếu","good":"Trung bình","strong":"Mạnh","mismatch":"Mật khẩu không khớp"};
 </script>
 <script id="password-strength-meter-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", { "locale_data": { "messages": { "": {} } } } );
+} )( "default", {"translation-revision-date":"2021-08-02 03:13:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
 <script src="https://votananhvu.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
 <script src="https://votananhvu.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
@@ -87,14 +87,14 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://votananhvu.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"c647e1d24e"};
+var userProfileL10n = {"user_id":"0","nonce":"709fb73d10"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", { "locale_data": { "messages": { "": {} } } } );
+} )( "default", {"translation-revision-date":"2021-08-02 03:13:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Hide":["Ẩn đi"],"Show":["Hiển thị"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."],"Hide password":["Ẩn mật khẩu"],"Show password":["Hiện mật khẩu"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
 <script src="https://votananhvu.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
 	<div class="clear"></div>
